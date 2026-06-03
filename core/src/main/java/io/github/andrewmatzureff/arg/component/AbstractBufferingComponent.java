735f4ef80664f147e64c079f2c44bfbd1f696f4d
AndrewMatzureff <andrewmatzureff@gmail.com>
2026-06-03 18:26:39 -0400

    [fix] Fix bug which caused teavm web app to crash due to initial render() delay

--- core/src/main/java/io/github/andrewmatzureff/arg/component/AbstractBufferingComponent.java
+++ core/src/main/java/io/github/andrewmatzureff/arg/component/AbstractBufferingComponent.java
@@ -10,18 +10,21 @@ import java.util.stream.Stream;
 public abstract class AbstractBufferingComponent<T> implements Streamable<T>, Component {
 
     protected final BufferedQueue<T> queue = new BufferedQueue<>();
-    private int frame = 1;
+    private long frame;
 
     public Stream<T> stream() {
+        frame = frame == 0 ? Gdx.graphics.getFrameId() : frame;
         if (frame == Gdx.graphics.getFrameId()) {
             frame++;
             preflush();
             return queue.flush().stream();
         }
 
-        throw new IllegalStateException("Tried to flush incremental queue data %s than once per frame! '%s::%s' must be called exactly once per frame (1 Frame = 1 Flush)."
+        throw new IllegalStateException("Tried to flush incremental queue data %s than once per frame! Streamed during %d frames despite LibGDX reporting %d have elapsed. '%s::%s' must be called exactly once per frame (1 Frame = 1 Flush)."
             .formatted(
                 frame > Gdx.graphics.getFrameId() ? "more" : "less",
+                frame,
+                Gdx.graphics.getFrameId(),
                 getClass().getSimpleName(),
                 "stream"));
     }
